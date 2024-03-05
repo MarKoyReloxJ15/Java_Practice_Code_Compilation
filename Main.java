4d7d131ab19062b0d045174ed45a315812addf3a
MarcosRel <marcosrelj15@gmail.com>
2024-03-05 12:28:11 +0800

    commit

--- Main.java
+++ Main.java
@@ -5,7 +5,24 @@ import java.util.*;
 public class Main {
 	
 	public void myfunction() {
+		int bilang = 14;
 		
+		int num = 0;
+		int num2= 0;
+		int a= 1;
+		for(int i=0;i<bilang;i++) {
+			
+			
+			num++;
+			
+			for(int j=0;j<num;j++) {
+				System.out.print(a);
+				a++;
+			}
+			
+			System.out.println();
+			
+		}
 	}
 	
 	
